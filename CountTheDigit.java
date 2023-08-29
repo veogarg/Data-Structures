14204f926e2a39ab85c8a9175e6d9e2a52d38a46
veogarg <vikas.91garg@gmail.com>
2023-08-29 21:50:08 +0530

    First Commit

---
+++ CountTheDigit.java
@@ -0,0 +1,17 @@
+class CountTheDigit {
+    public static void main(String[] args) {
+        int number = 123;
+        CountTheDigit reversedNumber = new CountTheDigit();
+        System.out.println(number);
+        System.out.println(reversedNumber.digitCount(number));
+    }
+
+    public int digitCount(int n) {
+        int res = 0;
+        while (n > 0) {
+            n = n / 10;
+            res++;
+        }
+        return res;
+    }
+}
